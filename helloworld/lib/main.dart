3ffce571c7bfcb2fbe7d3d2f30487cbba42426c4
Kyle <kyleedwardgjava@gmail.com>
2020-10-21 22:48:02 -0700

    Added What each widget does

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -7,15 +7,30 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( //Material App
       home: Scaffold(
         appBar: AppBar(
           title: Text("New Flutter App"),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text("Hello World"),
         ),
       ),
     );
   }
 }
+
+
+/*
+  Widgets Used:
+  - MaterialApp: The main core component of Flutter. Can access all the other
+    components and widgets provided by the Flutter SDK.
+  - Scaffold: Implements the basic material design visual layout structure.
+    Allows programmers to add basic widgets like an app bar, modify the body of the app
+  - Center: widgets that places widgets in the middle of the page
+  - Text: User's can add messeges in this widget
+
+
+
+
+ */
